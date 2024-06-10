e6fdf3bca6b9211f34141b440d158ec7cee87a74
noorsahar001 <noorsehar919@gmail.com>
2024-06-10 16:56:22 +0500

    Class_4

---
+++ data-types.js
@@ -0,0 +1,5 @@
+var myAge = 20;
+var myName = "Noor Sahar";
+var isJobless = false;
+console.log(myAge, myName, isJobless);
+console.log(myName.toUpperCase()); // Noor Sahar
---
+++ data-types.ts
@@ -0,0 +1,7 @@
+let myAge:number = 20;
+let myName:string = "Noor Sahar";
+let isJobless:boolean = false;
+
+console.log(myAge, myName, isJobless);
+
+console.log(myName.toUpperCase()) // Noor Sahar

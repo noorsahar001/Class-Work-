45824f514e390c2a87d5e9ccdcaeb40a843b1e64
noorsahar001 <noorsehar919@gmail.com>
2024-06-10 17:03:19 +0500

    Class_10

---
+++ fun.js
@@ -0,0 +1,32 @@
+// Basic Syntax
+// function functionName(param1, param2) {
+//  function body  
+//}
+var myFriends = ['aisha', 'areesha'];
+var unFriends = [];
+// function declaration with parameters
+function addFriend(name) {
+    // function body
+    // Push name to array
+    myFriends.push(name);
+}
+addFriend('usman');
+addFriend('sunny');
+console.log("--------------------------------------------");
+// Function with return type
+function removeFriend(name) {
+    // Remove name from array
+    var myNewFriends = [];
+    for (var _i = 0, myFriends_1 = myFriends; _i < myFriends_1.length; _i++) {
+        var friend = myFriends_1[_i];
+        if (friend !== name) {
+            myNewFriends.push(friend);
+        }
+        myFriends = myNewFriends;
+    }
+    return name;
+}
+var removedFriend = removeFriend('areesha');
+unFriends.push(removedFriend);
+console.log('myFriends', myFriends);
+console.log('unFriends', unFriends);

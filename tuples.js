d7a849990e438b812e1f95cd0695d6ccc324dbc4
noorsahar001 <noorsehar919@gmail.com>
2024-06-10 16:59:12 +0500

    Class_7

---
+++ tuples.js
@@ -0,0 +1,6 @@
+// create tuple 
+var ourTuple = [5, false, 'Coding God was here'];
+// We have no type safety in our tuple for indexes 3+
+ourTuple.push('Something new and wrong');
+ourTuple.push(100);
+console.log(ourTuple);
